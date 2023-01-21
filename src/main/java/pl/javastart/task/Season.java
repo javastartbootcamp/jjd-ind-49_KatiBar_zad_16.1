d6e1ba98082215440bf87c1ea27c54f1d3f68366
KatiBar <katarzyna.kinga.baranska@gmail.com>
2023-01-21 23:40:19 +0100

    Dodanie zawartości w klasie Season oraz metod w klasie SeasonManager

--- src/main/java/pl/javastart/task/Season.java
+++ src/main/java/pl/javastart/task/Season.java
@@ -1,5 +1,35 @@
 package pl.javastart.task;
 
 public enum Season {
-    //uzupełnij
+    SPRING("Wiosna", new String[]{"marzec", "kwiecień", "maj"}),
+    SUMMER("Lato", new String[]{"czerwiec", "lipiec", "sierpień"}),
+    AUTUMN("Jesień", new String[]{"wrzesień", "październik", "listopad"}),
+    WINTER("Zima", new String[]{"grudzień", "styczeń", "luty"});
+
+    private final String plName;
+    private final String[] months;
+
+    Season(String plName, String[] months) {
+        this.plName = plName;
+        this.months = months;
+    }
+
+    @Override
+    public String toString() {
+        return plName;
+    }
+
+    public String[] getMonths() {
+        return months;
+    }
+
+    public static Season changeStringToSeason(String season) {
+        for (Season value : Season.values()) {
+            if (season.compareToIgnoreCase(value.plName) == 0) {
+                return value;
+            }
+        }
+        return null;
+    }
+
 }
